0b7ff52c4123d15ec0f8567b6892dc56aade15e4
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2022-03-27 12:35:55 -0700

    Finish initial marketplace setup

---
+++ src/backend/contracts/Marketplace.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Marketplace is ReentrancyGuard {
+  
+  // State Variables
+  address payable public immutable feeAccount; // the account that receives fees
+  uint public immutable feePercent; // the fee percentage on sales
+  uint public itemCount;
+
+  constructor(uint _feePercent) {
+    feeAccount = payable(msg.sender);
+    feePercent = _feePercent;
+  }
+}
