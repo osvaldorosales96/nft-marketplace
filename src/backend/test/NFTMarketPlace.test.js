c1c2054cde45d972029b48cb4c60f9a7db3b5944
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2022-03-27 13:33:18 -0700

    Sample test for nft deployment and marketplacd setup DONE

--- src/backend/test/NFTMarketPlace.test.js
+++ src/backend/test/NFTMarketPlace.test.js
@@ -4,6 +4,7 @@ const { ethers } = require("hardhat");
 describe("NFTMarketplace", function(){
   let deployer, addr1, addr2, nft, marketplace
   let feePercent = 1
+  let URI = "Sample URI"
   beforeEach(async function() {
     // Get contract factories
     const NFT = await ethers.getContractFactory("NFT");
@@ -14,10 +15,34 @@ describe("NFTMarketplace", function(){
     nft = await NFT.deploy();
     marketplace = await Marketplace.deploy( feePercent );
   });  
+
   describe("Deployment", function () {
+
     it("Should track name and symbol of the nft collection", async function () {
       expect(await nft.name()).to.equal("DApp NFT");
       expect(await nft.symbol()).to.equal("DAPP");
     });
+
+    it("Should track feeAccount and feePercent of the marketplace", async function () {
+      expect(await marketplace.feeAccount()).to.equal(deployer.address);
+      expect(await marketplace.feePercent()).to.equal(feePercent);
+    });
+  });
+
+  describe("Minting NFTs", function (){
+    it("Should track each minted NFT", async function() {
+      // addr1 mints nft
+      await nft.connect(addr1).mint(URI)
+      expect(await nft.tokenCount()).to.equal(1);
+      expect(await nft.balanceOf(addr1.address)).to.equal(1);
+      expect(await nft.tokenURI(1)).to.equal(URI);
+
+      // addr2 mints nft
+      await nft.connect(addr2).mint(URI)
+      expect(await nft.tokenCount()).to.equal(2);
+      expect(await nft.balanceOf(addr2.address)).to.equal(1);
+      expect(await nft.tokenURI(2)).to.equal(URI);
+    })
+
   });
 })
