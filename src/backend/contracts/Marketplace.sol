24fb8b17b46a38a73ffddb9c3bb3dc472725cf61
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2022-03-27 13:49:30 -0700

    marketplace added makeItem function

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -11,8 +11,55 @@ contract Marketplace is ReentrancyGuard {
   uint public immutable feePercent; // the fee percentage on sales
   uint public itemCount;
 
+  struct item{
+    uint itemId;
+    IERC721 nft;
+    uint tokenId;
+    uint price;
+    address payable seller;
+    bool sold;
+  }
+
+  event Offered(
+    uint itemId,
+    address indexed nft,
+    uint tokenId,
+    uint price,
+    address indexed seller
+  );
+
+  // itemId -> Item
+  mapping(uint => Item) public items;
+
   constructor(uint _feePercent) {
     feeAccount = payable(msg.sender);
     feePercent = _feePercent;
+  }
+
+  function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
+    require(_price > 0, "Price must be greater that zero");
+    // Increment itemCount
+    itemCount ++;
+    // Transfer nft
+    _nft.transgerFrom(msg.sender, address(this), _tokenId);
+    // Add new item to items mapping
+    items[itemCount] = Item(
+      itemCount,
+      _nft,
+      _tokenId,
+      _price,
+      payable(msg.sender),
+      false
+    );
+    // emit Offered event
+    emit Offered(
+      itemCount, 
+      address(_nft), 
+      _tokenId, 
+      _price, 
+      msg.sender
+    );
+    
+
   }
 }
