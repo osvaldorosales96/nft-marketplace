3f3e41f6f7fad2954d8db95dbfd834e47c52ebd5
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2022-03-29 22:18:41 -0700

    Finished Navbar, home page and metamask

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,25 +1,24 @@
-
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
-import logo from './logo.png';
-import './App.css';
 import Navigation from './Navbar';
+import Home from './Home.js'
+import Create from './Create.js'
+import MyListedItems from './MyListedItems.js'
+import MyPurchases from './MyPurchases.js'
+
+import MarketplaceAddress from '../contractsData/Marketplace-address.json'
+import MarketplaceAbi from '../contractsData/Marketplace.json'
+import NFTAbi from '../contractsData/NFT.json'
+import NFTAddress from '../contractsData/NFT-address.json'
 
 import { useState } from 'react'
 import { ethers } from "ethers"
-import MarketplaceAddress from '../contractsData/Marketplace-address.json'
-import MarketplaceABI from '../contractsData/Marketplace.json'
-import NFTAbi from '../contractsData/NFT-address.json'
-import NFTAddress from '../contractsData/NFT-address.json'
+import { Spinner } from 'react-bootstrap'
 
-import Home from './Home'
-import Create from './Create'
-import MyListedItems from './MyListedItems'
-import MyPurchases from './MyPurchases'
-import {Spinner} from 'react-bootstrap'
+import './App.css';
  
 function App() {
   const [loading, setLoading] = useState(true)
@@ -27,20 +26,29 @@ function App() {
   const [nft, setNFT] = useState({})
   const [marketplace, setMarketplace] = useState({})
 
-  // Metamask login/connect
+  // MetaMask Login/Connect
   const web3Handler = async () => {
-    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});  
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts[0])
     // Get provider from Metamask
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     // Set signer
     const signer = provider.getSigner()
 
+    window.ethereum.on('chainChanged', (chainId) => {
+      window.location.reload();
+    })
+
+    window.ethereum.on('accountsChanged', async function (accounts) {
+      setAccount(accounts[0])
+      await web3Handler()
+    })
     loadContracts(signer)
   }
 
   const loadContracts = async (signer) => {
     // Get deployed copies of contracts
-    const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceABI.abi, signer)
+    const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer)
     setMarketplace(marketplace)
     const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer)
     setNFT(nft)
@@ -50,9 +58,12 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div>
-        <Navigation web3Handler={web3Handler} account={account} />
-        {loading ? (
+      <div className="App">
+        <>
+          <Navigation web3Handler={web3Handler} account={account} />
+        </>
+        <div>
+          {loading ? (
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
               <Spinner animation="border" style={{ display: 'flex' }} />
               <p className='mx-3 my-0'>Awaiting Metamask Connection...</p>
@@ -62,18 +73,12 @@ function App() {
               <Route path="/" element={
                 <Home marketplace={marketplace} nft={nft} />
               } />
-              <Route path="/create" element={
-                <Create marketplace={marketplace} nft={nft} />
-              } />
-              <Route path="/my-listed-items" element={
-                <MyListedItems marketplace={marketplace} nft={nft} account={account} />
-              } />
-              <Route path="/my-purchases" element={
-                <MyPurchases marketplace={marketplace} nft={nft} account={account} />
-              } />
+              <Route path="/create"  />
+              <Route path="/my-listed-items" />
+              <Route path="/my-purchases"  />
             </Routes>
           )}
-
+        </div>
       </div>
     </BrowserRouter>
       
